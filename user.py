e0860310a3f12b3ba4f9c0ed4820294c805f21a5
williammango <williammango2015@gmail.com>
2019-06-15 10:36:48 +0300

    create variables to store objects

--- user.py
+++ user.py
@@ -1,16 +1,18 @@
-import pyperclip
-
 
 class User:
-    """docstring for ."""
+    """  Class that generates new instances of users."""
+    user = []
 
-    def __init__(self, arg):
-        super(, self).__init__()
-        self.arg = arg
+    def __init__(self, sir_name, other_name, phone, email):
+        """ helps define properties of our objects"""
+        self.first_name = sir_name
+        self.other_name = other_name
+        self.phone = phone
+        self.email = email
 
 
 class Credentials:
-    """docstring for ."""
+    """Class that generates new instances of credentials."""
 
     def __init__(self, arg):
         super(, self).__init__()
