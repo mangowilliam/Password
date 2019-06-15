19a73aef195674555c247eba27ed7803a06644a8
williammango <williammango2015@gmail.com>
2019-06-15 10:13:52 +0300

    create user and credential class

--- user.py
+++ user.py
@@ -1,1 +1,17 @@
+import pyperclip
+
+
 class User:
+    """docstring for ."""
+
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
+
+
+class Credentials:
+    """docstring for ."""
+
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
