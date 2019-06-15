f395284f5b6fd57f951e63d1c2ba7a0c5a9d5423
williammango <williammango2015@gmail.com>
2019-06-15 21:48:18 +0300

    perform 1st tes

--- user.py
+++ user.py
@@ -5,15 +5,11 @@ class User:
 
     def __init__(self, sir_name, other_name, phone, email):
         """ helps define properties of our objects"""
-        self.first_name = sir_name
+        self.sir_name = sir_name
         self.other_name = other_name
         self.phone = phone
         self.email = email
 
 
-class Credentials:
-    """Class that generates new instances of credentials."""
-
-    def __init__(self, arg):
-        super(, self).__init__()
-        self.arg = arg
+# class Credentials:
+#    """Class that generates new instances of credentials."""
--- user_test.py
+++ user_test.py
@@ -1,5 +1,33 @@
 import unittest
-from user import user
+from user import User
 
 
-class TestUser(unnittest.TestCase):
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+    def setUp(self):
+        """
+        set up method to run before each test case
+        """
+        self.new_user = User("Mango", "William", "0770771045",
+                             "juniormango@yahoo.com")  # new user objects
+
+    def test_init_new_user(self):
+        """
+        test_init test case to test if each object is initialized correctly
+        """
+        self.assertEqual(self.new_user.sir_name, "Mango")
+        self.assertEqual(self.new_user.other_name, "William")
+        self.assertEqual(self.new_user.phone, "0770771045")
+        self.assertEqual(self.new_user.email, "juniormango@yahoo.com")
+
+    de
+
+
+if __name__ == '__main__':
+    unittest.main()
